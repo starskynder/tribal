761039c39e7046d4f23d0685e867c38020b7114e
starskynder <witchywisher@gmail.com>
2017-01-31 19:40:34 +0100

    Add files via upload

---
+++ tribal.js
@@ -0,0 +1,8 @@
+$(function() {
+    $("#show-menu").click(function() {
+        $(".navbar").css("left", "0px");
+    })
+    $("#closing-x").click(function() {
+        $(".navbar").css("left", "-250px");
+    })
+}) //end ready
